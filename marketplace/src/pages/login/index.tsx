8db3c0695fa06ddaf1d9fb2b1488bc3e359b15a7
JoseRandoDEV <joseandresrando@gmail.com>
2025-06-04 23:47:22 -0300

    Login (Clase 4)

--- marketplace/src/pages/login/index.tsx
+++ marketplace/src/pages/login/index.tsx
@@ -1,12 +1,41 @@
 import React from "react"
-import { Button, Container } from "@mui/material"
+import { Box, Button, Container, Paper, TextField, Typography, Grid } from "@mui/material"
 
-export const LoginPage: React.FC<{}> = () =>{
-    return(
-        <Container sx={{mt: 9}} maxWidth="xl">
-        <Button fullWidth variant='contained'>
-          Estamos en la Pagina LOGIN
-        </Button>
-      </Container>
-    )
+type LoginType = {
+  username: string;
+  password: string;
+}
+
+export const LoginPage: React.FC<{}> = () => {
+  const [loginData , setLoginData] = React.useState<LoginType>({
+    username: "",
+    password: ""
+  });
+
+  const dataLogin = (e: React.ChangeEvent<HTMLInputElement>) =>{
+    setLoginData({...loginData, [e.target.name]: e.target.value});
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(loginData)
+  }
+
+  return (
+    <Container maxWidth="sm">
+      <Grid container direction="column" alignItems="center" justifyContent="center" 
+      sx={{ minHeight: "100vh" }}>
+       <Grid>
+          <Paper sx={{ padding: "1.2em", borderRadius: "0.5em" }}>
+            <Typography sx={{mt:1, mb: 1}} variant="h4">Iniciar Sesion</Typography>
+            <Box component="form" onSubmit={handleSubmit}>
+              <TextField name="username" margin="normal" type="text" fullWidth label="Email" sx={{mt:2, mb: 1.5}} required onChange={dataLogin}/>
+              <TextField name="password" margin="normal" type="password"fullWidth label="Password" sx={{mt:1.5, mb: 1.5}} required onChange={dataLogin}/>
+              <Button fullWidth type="submit" variant="contained" sx={{mt:3, mb: 1.5}}>Iniciar Sesion</Button>
+            </Box>
+          </Paper>
+        </Grid>
+      </Grid>
+    </Container>
+  )
 }
